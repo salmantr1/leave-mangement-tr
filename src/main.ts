a4fd9b57201e33c5a1ab4debd9a4d1c68727fd17
salman <sashraf@technologyrivers.com>
2025-06-16 12:53:56 +0500

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UsersService } from './users/users.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-// Removed CallbackHandler import as it is not exported by '@vercel/node'
 
-let server: any;
-
-async function bootstrap(): Promise<any> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const usersService = app.get(UsersService);
   await usersService.createAdmin();
@@ -30,19 +24,9 @@ async function bootstrap(): Promise<any> {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.init();
-  return app.getHttpAdapter().getInstance();
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`App is running on http://localhost:${port}`);
 }
 
-// Only run listener locally
-if (!process.env.VERCEL) {
-  bootstrap().then((app) => {
-    app.listen(process.env.PORT ?? 3000);
-  });
-}
-
-// Vercel entry point
-export default async function handler(...args: any[]) {
-  server = server ?? (await bootstrap());
-  return server(...args);
-}
+bootstrap();
